f100226b233d40b3ad16f8f9b9b3a7cfb30b1e46
zz1998022 <1074045407@qq.com>
2021-07-07 01:35:57 +0800

    :beers:添加mysql配置项

--- anime-blog-server/config/plugin.ts
+++ anime-blog-server/config/plugin.ts
@@ -6,6 +6,21 @@ const plugin: EggPlugin = {
   //   enable: true,
   //   package: 'egg-view-nunjucks',
   // },
+  
+  // 配置jwt插件
+  jwt: {
+    enable: true,
+    package: 'egg-jwt'
+  },
+  cors: {
+    enable: true,
+    package: 'egg-cors'
+  },
+  // 配置mysql
+  mysql: {
+    enable: true,
+    package: 'egg-mysql'
+  }
 };
 
 export default plugin;
