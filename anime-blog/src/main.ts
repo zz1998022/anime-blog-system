9bb3799b6b903879642015bdc860856d717f0eac
NekoChanTW <aaron74327@gmail.com>
2021-07-06 16:35:32 -0700

    移除 antd.css 、 使用 use 代替遍歷

--- anime-blog/src/main.ts
+++ anime-blog/src/main.ts
@@ -1,16 +1,15 @@
 import { createApp } from 'vue'
 import { router } from './router'
 import { Layout, Typography, Menu } from 'ant-design-vue'
-import 'ant-design-vue/dist/antd.css'
 import App from './App.vue'
 
-const app = createApp(App)
+createApp(App)
+  // 路由
+  .use(router)
 
-const antUi = [Layout, Typography, Menu]
-antUi.forEach(ui => {
-  app.use(ui)
-})
+  // Ant Design Vue
+  .use(Layout)
+  .use(Typography)
+  .use(Menu)
 
-app.use(router)
-
-app.mount('#app')
+  .mount('#app')
