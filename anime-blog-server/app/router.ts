54d4f678ce39455bd77e94a9c737339531f2f829
Lemon <1074045407@qq.com>
2021-07-09 14:54:47 +0800

    :memo:更正错误的单词

--- anime-blog-server/app/router.ts
+++ anime-blog-server/app/router.ts
@@ -8,5 +8,5 @@ export default (app: Application) => {
   // 获取文章数据
   router.get('/article',controller.article.getArticle);
   // 获取最近文章
-  router.get('/article/lately',controller.article.getRecentArticles);
+  router.get('/article/recently',controller.article.getRecentArticles);
 };
