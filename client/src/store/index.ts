70ea7d97225509bde72274af5f4bde654cdfeb7b
NekoChanTW <aaron74327@gmail.com>
2021-07-11 22:56:10 -0700

    移除 vuex

--- client/src/store/index.ts
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'vuex'
-import { actions } from './action'
-import { mutations } from './mutations'
-
-export const store = createStore({
-  state: {},
-  actions,
-  mutations,
-})
