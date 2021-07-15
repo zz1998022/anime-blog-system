c1a565799b13025963de9963cc6da94b8698fd3a
NekoChanTW <aaron74327@gmail.com>
2021-07-14 18:53:49 -0700

    優化業務邏輯語法
    - 使用短路運算符取代 if 語句
    - 拆開不必要的 if 嵌套

--- client/src/views/HomePage/hooks/useGetTitle.ts
+++ client/src/views/HomePage/hooks/useGetTitle.ts
@@ -7,7 +7,7 @@ const config = {
   delete: 50, // 刪除速度 (毫秒)
   switch: 1500, // 從 輸入完成 到 開始刪除 的間隔 (毫秒)
   next: 300, // 刪除完成後 切換到 下一個副標題　的間隔 (毫秒)
-  mode: 'order', // 模式: 'order'(順序), 'random'(隨機)，當 subTitleData.length 大於 1 時生效
+  mode: 'random', // 模式: 'order'(順序), 'random'(隨機)，當 subTitleData.length 大於 1 時生效
 }
 
 export const useGetTitle = () => {
@@ -17,7 +17,6 @@ export const useGetTitle = () => {
   // TODO: 輸入完成 到 開始刪除 的間隔 添加一個 class 讓 光標有閃爍的動畫
 
   // 副標題打字效果
-  // ! 疑似有性能問題
   /**
    * @function handleSubTitle
    * 調用核心函式前的模式處理及基本聲明變量
@@ -25,29 +24,29 @@ export const useGetTitle = () => {
    * @param {string} subTitleData - 副標題陣列
    */
   function handelSubTitle(subTitleData: string[], mode: string = 'order') {
-    const subTitleDataLength = subTitleData.length
-    const subTitleDataIndexLength = subTitleDataLength - 1
+    const subTitleDataLength = subTitleData.length // 副標題資料長度
+    const subTitleDataIndexLength = subTitleDataLength - 1 // 索引值總長度
     let nextOrder = 0 // 順序模式下一個索引值
-    // 檢查數量再啟用模式
-    if (subTitleDataLength > 1) {
-      switch (mode) {
-        case 'order':
-          // 順序
-          meow(subTitleData[0])
-          nextOrder++
-          break
-        case 'random':
-          // 隨機
-          meow(subTitleData[_random(0, subTitleDataIndexLength, false)])
-          break
-      }
-    } else {
-      meow(subTitleData[0])
+
+    // 若數量等於 1 則指定索引值為 0
+    if (subTitleDataLength === 1) return meow(subTitleData[0])
+
+    // 模式
+    switch (mode) {
+      case 'order':
+        // 順序
+        meow(subTitleData[0])
+        nextOrder++
+        break
+      case 'random':
+        // 隨機
+        meow(subTitleData[_random(0, subTitleDataIndexLength, false)])
+        break
     }
 
     /**
      * @function meow - 核心函數
-     *  * 處理打字頁面的打字效果，也包括調用下一次核心函數的模式處理
+     *  * 處理打字效果，也包括調用下一次核心函數的模式處理
      * @param {string} subTitle - 副標題
      */
     function meow(title: string) {
@@ -57,7 +56,7 @@ export const useGetTitle = () => {
         for (let i = 0; i < titleLen; i++) {
           setTimeout(() => {
             subTitle.value += title[i]
-            if (i === titleLen - 1) setTimeout(() => resolve(null), config.switch)
+            i === titleLen - 1 && setTimeout(() => resolve(null), config.switch)
           }, i * config.type)
         }
       })
@@ -68,18 +67,22 @@ export const useGetTitle = () => {
               setTimeout(() => {
                 subTitle.value = subTitle.value.split('').slice(0, -1).join('')
                 // 下一個
-                if (i === titleLen - 1) setTimeout(() => resolve(null), config.next)
+                i === titleLen - 1 && setTimeout(() => resolve(null), config.next)
               }, i * config.delete)
             }
           })
         })
         .then(() => {
           // 調用下一次前的模式處理
-          if (subTitleDataLength === 0) return meow(subTitleData[0]) // 長度為 0 時，直接使用第一個值
+
+          // 若數量等於 1 則指定索引值為 0
+          if (subTitleDataLength === 1) return meow(subTitleData[0])
+
+          // 模式
           switch (mode) {
             case 'order':
               // 順序
-              if (nextOrder === subTitleDataLength) nextOrder = 0 // 超過限制，重置
+              nextOrder === subTitleDataLength && (nextOrder = 0) // 超出長度，重置索引值
               meow(subTitleData[nextOrder])
               nextOrder++
               break
@@ -92,7 +95,7 @@ export const useGetTitle = () => {
     }
   }
   onMounted(() => {
-    handelSubTitle(subTitleData, 'random')
+    handelSubTitle(subTitleData, config.mode)
   })
 
   return { subTitle }
